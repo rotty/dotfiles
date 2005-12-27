440933f8d302ff78de15e1c3566f5e9888830283
a.rottmann <a.rottmann@gmx.at>
2005-12-27 15:59:00 +0000

    Emacs: trim-down .emacs.el
    darcs-hash:20051227155910-1cc0d-86a306f21bf4bb53ba51db9941e4cfb9ed886acd.gz

--- .emacs.el
+++ .emacs.el
@@ -1,33 +1,52 @@
 ;; -*- Mode: Emacs-Lisp -*-
 
+;; enable some commands disabled by default
 (put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
-;; Not bound anymore by default?
-(global-set-key [home] 'beginning-of-line)
-(global-set-key [end] 'end-of-line)
+;; Use UTF-8 for file name encoding
+(setq file-name-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 
-;;
+;; No fancy graphic stuff, you don't want to use the mouse anyway, do
+;; you?
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+
 ;; http://opal.cabochon.com/~stevey/blog-rants/effective-emacs.html
-
 (global-set-key "\C-w"     'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
 (global-set-key [f5]  'call-last-kbd-macro)
+
+;; Some other requently-used functions
+(global-set-key [home] 'beginning-of-line)
+(global-set-key [end] 'end-of-line)
+
 (global-set-key [f11] 'goto-line)
 (global-set-key [f12] 'toggle-transient-mark-mode)
 
+;; Enhanced buffer switching
 (iswitchb-mode t)
+
+;; Syntax coloring
 (global-font-lock-mode t)
 
-;; I do everything manually
+;; Paren matching
+(show-paren-mode t)
+
+;; I do configuration manually; the customization feature just gets in
+;; the way of modularizing your emacs configuation, IMHO
 (setq custom-file nil)
+
+
+(add-hook 'mail-setup-hook 'mail-abbrevs-setup)
+(define-key emacs-lisp-mode-map "\C-xx" 'edebug-defun)
 
 ;; Extend load-path
 (let ((top (expand-file-name "~/.emacs.d/lisp")))
@@ -38,46 +57,3 @@
 
 (dolist (snippet '("scheme" "cplus" "slime48" "tramp" "crypt"))
   (load (expand-file-name (concat"~/.emacs.d/config/" snippet ".el"))))
-
-;;; ****************
-;;: Dictionary
-;;;;
-(global-set-key [f3] 'dictionary-lookup-definition)
-
-;;; ******************
-;;; Mail
-;;;
-(add-hook 'mail-setup-hook 'mail-abbrevs-setup)
-
-;;; ********************
-;;; Edebug is a source-level debugger for emacs-lisp programs.
-;;;
-(define-key emacs-lisp-mode-map "\C-xx" 'edebug-defun)
-
-;; show paren mode
-(show-paren-mode t)
-
-;; (autoload 'newsticker-start "newsticker" "Emacs Newsticker" t)
-;; (autoload 'newsticker-show-news "newsticker" "Emacs Newsticker" t)
-
-;; (setq newsticker-url-list
-;;       '(("Planet Scheme" "http://planet-scheme.yi.org/rss20.xml")))
-
-;; Diary
-(add-hook 'diary-display-hook 'fancy-diary-display)
-(add-hook 'list-diary-entries-hook 'sort-diary-entries)
-(add-hook 'list-diary-entries-hook 'include-other-diary-files)
-(add-hook 'mark-diary-entries-hook 'mark-included-diary-files)
-
-
-
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-
-;; Use UTF-8 for file name encoding
-(setq file-name-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-;; Load saved Emacs session
-;;(desktop-load-default)
-;;(desktop-read)
