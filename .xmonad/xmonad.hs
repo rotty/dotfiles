1629114b7f5f8966a85a4e5f935d9b1a24d28762
Andreas Rottmann <a.rottmann@gmx.at>
2008-03-30 22:15:48 +0200

    xmonad: Added some keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,8 @@ import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.CycleWS
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -11,12 +13,15 @@ myKeys (XConfig {modMask = modm}) =
     M.fromList $
          [ ((modm,               xK_Up    ), windows W.focusUp)
          , ((modm,               xK_Down  ), windows W.focusDown)
+         , ((modm .|. shiftMask, xK_Down  ), windows W.swapDown )
+         , ((modm .|. shiftMask, xK_Up    ), windows W.swapUp )
          , ((modm,               xK_Right ), sendMessage (IncMasterN 1))
          , ((modm,               xK_Left  ), sendMessage (IncMasterN (-1)))
          , ((modm,               xK_period), nextWS )
          , ((modm,               xK_comma ), prevWS )
          , ((modm .|. shiftMask, xK_comma ), shiftToPrev )
-         , ((modm .|. shiftMask, xK_period), shiftToNext )]
+         , ((modm .|. shiftMask, xK_period), shiftToNext )
+         , ((modm,               xK_p),      shellPrompt defaultXPConfig )]
 
 main = xmonad $ defaultConfig 
        { manageHook       = manageDocks <+> manageHook defaultConfig
