3b89ff05d1351d15b2ddabbebce1f6b753c35c3d
Andreas Rottmann <a.rottmann@gmx.at>
2008-10-03 16:12:10 +0200

    xmonad: configure searching and 'UpdatePointer'

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,9 +4,12 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.CycleWS
 import XMonad.Prompt
 import XMonad.Prompt.Shell
+import XMonad.Actions.UpdatePointer
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
+import qualified XMonad.Actions.Submap as SM
+import qualified XMonad.Actions.Search as S
 
 myKeys :: XConfig t -> M.Map (KeyMask, KeySym) (X ())
 myKeys (XConfig {modMask = modm}) =
@@ -21,11 +24,22 @@ myKeys (XConfig {modMask = modm}) =
          , ((modm,               xK_comma ), prevWS )
          , ((modm .|. shiftMask, xK_comma ), shiftToPrev )
          , ((modm .|. shiftMask, xK_period), shiftToNext )
-         , ((modm,               xK_p),      shellPrompt defaultXPConfig )]
+         , ((modm,               xK_p),      shellPrompt defaultXPConfig )
+         -- Search commands
+         , ((modm, xK_s), SM.submap $ searchEngineMap 
+                            $ S.promptSearchBrowser defaultXPConfig "iceweasel")
+         , ((modm .|. shiftMask, xK_s), SM.submap $ searchEngineMap $ S.selectSearch)
+         ]
+
+searchEngineMap method = M.fromList $
+                         [ ((0, xK_g), method S.google)
+                         , ((0, xK_c), method S.codesearch)
+                         , ((0, xK_w), method S.wikipedia)
+                         ]
 
 main = xmonad $ defaultConfig 
        { manageHook       = manageDocks <+> manageHook defaultConfig
-       , logHook          = ewmhDesktopsLogHook
+       , logHook          = ewmhDesktopsLogHook >> updatePointer Nearest
        , layoutHook       = ewmhDesktopsLayout $ avoidStruts $ layoutHook defaultConfig
        , modMask          = mod4Mask
        , keys             = \c -> myKeys c `M.union` keys defaultConfig c
