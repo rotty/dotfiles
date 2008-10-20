1f481f4e1a2e60633d5993232b1af24affc120f5
Andreas Rottmann <a.rottmann@gmx.at>
2008-10-20 16:24:50 +0200

    emacs (scheme.el): snarf stuff from TRC

--- .emacs.d/config/scheme.el
+++ .emacs.d/config/scheme.el
@@ -40,6 +40,10 @@
 (setq quack-pltish-keywords-to-fontify
       '("and" "begin" "call-with-current-continuation" "call-with-input-file" "call-with-output-file" "call/cc" "case" "case-lambda" "compound-unit/sig" "cond" "condition" "cond-expand" "define" "define/optional" "define-condition-type" "define-macro" "define-module" "define-public" "define-signature" "define-syntax" "define-syntax-set" "define-values" "define-values/invoke-unit/sig" "define-method" "define-generic" "define-class" "delay" "do" "else" "exit-handler" "guard" "if" "import" "lambda" "let" "let*" "let*-values" "let+" "let-keywords" "let-optional" "let-syntax" "let-values" "let/ec" "letrec" "letrec-values" "letrec-syntax" "library" "match-lambda" "match-lambda*" "match-let" "match-let*" "match-letrec" "match-define" "mixin" "opt-lambda" "or" "override" "override*" "namespace-variable-bind/invoke-unit/sig" "parameterize" "private" "private*" "protect" "provide" "provide-signature-elements" "provide/contract" "public" "public*" "quote" "receive" "rename" "require" "require-for-syntax" "send" "send*" "setter" "set!" "set!-values" "signature->symbols" "super-instantiate" "syntax-case" "syntax-case*" "syntax-error" "syntax-rules" "unit/sig" "unless" "when" "with-handlers" "with-method" "with-syntax"))
 
+(defun ikarus-toggle-trace ()
+  ;; IMPLEMENTME
+  t)
+
 (dolist (hint
 	 '((with-test-prefix 1)
 	   (with-interaction-environment 1)
@@ -103,5 +107,178 @@
 	   (library 1)
 	   (trace-define 1)
 	   (trace-lambda 2)
-	   (and-let* 1)))
+	   (and-let* 1)
+	   (let-attributes 3)
+	   (let-accessors 2)))
   (put (car hint) 'scheme-indent-function (cadr hint)))
+
+(eval-after-load "scheme" '(progn
+
+;;; This is a *slightly* modified version of what is in scheme.el,
+;;; which is itself a slight modification of `lisp-indent-function'
+;;; from lisp-mode.el.  Gee, you'd think that someone would think of
+;;; the notion of 'abstraction' here...
+
+(defun scheme-indent-function (indent-point state)
+  (let ((normal-indent (current-column)))
+    (goto-char (1+ (elt state 1)))
+    (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+    (if (and (elt state 2)
+             (not (looking-at "\\sw\\|\\s_")))
+        ;; car of form doesn't seem to be a symbol
+        (progn
+          (if (not (> (save-excursion (forward-line 1) (point))
+                      calculate-lisp-indent-last-sexp))
+              (progn (goto-char calculate-lisp-indent-last-sexp)
+                     (beginning-of-line)
+                     (parse-partial-sexp (point)
+                                         calculate-lisp-indent-last-sexp 0 t)))
+          ;; Indent under the list or under the first sexp on the same
+          ;; line as calculate-lisp-indent-last-sexp.  Note that first
+          ;; thing on that line has to be complete sexp since we are
+          ;; inside the innermost containing sexp.
+          (backward-prefix-chars)
+          (current-column))
+      (let ((function (downcase         ;** downcasage added by TRC
+                       (buffer-substring (point)
+                                         (progn (forward-sexp 1) (point)))))
+            method)
+        (setq method (or (get (intern-soft function) 'scheme-indent-function)
+                         (get (intern-soft function) 'scheme-indent-hook)))
+        (cond ((or (eq method 'defun)
+                   (and (null method)
+                        (> (length function) 3)
+                        (string-match "\\`def" function)))
+               (lisp-indent-defform state indent-point))
+              ;** WITH-... & CALL-WITH-... forms added by TRC
+              ((or (eq method 'with-...)
+                   (eq method 'call-with-...)
+                   (and (null method)
+                        (or (and (> (length function) 5)
+                                 (string-match "\\`with-" function))
+                            (and (> (length function) 9)
+                                 (string-match "\\`call-with-" function)))))
+               (lisp-indent-withform state indent-point))
+              ((integerp method)
+               (lisp-indent-specform method state
+                                     indent-point normal-indent))
+              (method
+               (funcall method state indent-point normal-indent)))))))
+
+;;; This could be generalized to negative special form indent methods; e.g.,
+;;;
+;;;   (put 'with-frobbotzim 'scheme-indent-function -2)
+;;;
+;;; and then
+;;;
+;;;   (with-frobbotzim frob grovel
+;;;       full lexical
+;;;       mumble chumble
+;;;       spuzz
+;;;     (lambda (foo) ...)
+;;;     (lambda (bar) ...))
+;;;
+;;; That is, the last two subforms would be indented two spaces, whereas all
+;;; preceding subforms would get four spaces.
+
+(defun lisp-indent-withform (state indent-point)
+  (if (not (and (boundp 'paredit-mode)
+                paredit-mode))
+      ;; If we're not in paredit mode, it's not really safe to go backwards
+      ;; from the end and to try to indent based on that, since there may not
+      ;; be an end to work backwards from (i.e. the structure not be valid).
+      (lisp-indent-defform state indent-point)
+    (goto-char (nth 1 state))
+    (let ((body-column (+ (current-column)
+                          lisp-body-indent)))
+      (forward-sexp 1)
+      (backward-char 1)
+      (backward-sexp 1)
+      (skip-chars-backward " \t" (point-at-bol))
+      (if (= (point) indent-point)
+          body-column
+          ;; If it's not the last argument, then we must specify not only the
+          ;; column to indent to but also the start of the containing sexp,
+          ;; which implies (don't ask me how) that any *following* subforms
+          ;; must be indented separately, and not just on this column.  This
+          ;; allows C-M-q to know to indent the penultimate arguments with four
+          ;; spaces, but to keep recomputing the indentation so that it doesn't
+          ;; assume the last one will go to the same column, which is a wrong
+          ;; assumption.
+          (list (+ body-column lisp-body-indent)
+                (nth 1 state))))))
+
+(put 'let-fluids 'scheme-indent-function 'with-...)
+
+;;; Nested foof-loop forms
+
+(put 'iterate 'scheme-indent-function 'with-...)
+(put 'iterate! 'scheme-indent-function 'with-...)
+(put 'iterate* 'scheme-indent-function 'with-...)
+(put 'iterate-values 'scheme-indent-function 'with-...)
+(put 'lazy-recur 'scheme-indent-function 'with-...)
+(put 'lazy-recur* 'scheme-indent-function 'with-...)
+(put 'recur 'scheme-indent-function 'with-...)
+(put 'recur* 'scheme-indent-function 'with-...)
+
+;;; This is silly, but so would altering the definition of
+;;; `scheme-indent-function' yet again to include a test for
+;;; `collect-...'.  Better would be to have a table mapping regular
+;;; expressions to indent functions, as Edwin has.  But this is
+;;; expedient for now.
+
+(put 'collect-average 'scheme-indent-function 'with-...)
+(put 'collect-display 'scheme-indent-function 'with-...)
+(put 'collect-list 'scheme-indent-function 'with-...)
+(put 'collect-list! 'scheme-indent-function 'with-...)
+(put 'collect-list-into! 'scheme-indent-function 'with-...)
+(put 'collect-list-reverse 'scheme-indent-function 'with-...)
+(put 'collect-max 'scheme-indent-function 'with-...)
+(put 'collect-min 'scheme-indent-function 'with-...)
+(put 'collect-product 'scheme-indent-function 'with-...)
+(put 'collect-stream 'scheme-indent-function 'with-...)
+(put 'collect-string 'scheme-indent-function 'with-...)
+(put 'collect-string-of-length 'scheme-indent-function 'with-...)
+(put 'collect-sum 'scheme-indent-function 'with-...)
+(put 'collect-vector 'scheme-indent-function 'with-...)
+(put 'collect-vector-of-length 'scheme-indent-function 'with-...)
+
+;;; This one doesn't follow the same pattern as the others, because
+;;; there is no expression; (COLLECT-COUNT ...) is the same as
+;;; (COLLECT-SUM ... 1).
+
+(put 'collect-count 'scheme-indent-function 0)
+
+;;;; RECEIVE Indentation
+
+;;; (RECEIVE <bvl>           ; Line up BVL & producer.
+;;;          <producer>
+;;;   <body>)
+
+(defun scheme-indent-receive (state indent-point normal-indent)
+  (let ((containing-form-start (nth 1 state))
+        (i 0)
+        containing-form-column)
+    ;; <snip documentation>
+    (goto-char containing-form-start)
+    (setq containing-form-column (current-column))
+    (forward-char 1)
+    (forward-sexp 1)
+    ;; Now find the start of the last form.
+    (parse-partial-sexp (point) indent-point 1 t)
+    (while (and (< (point) indent-point)
+                (condition-case ()
+                    (progn
+                      (setq i (1+ i))
+                      (forward-sexp 1)
+                      (parse-partial-sexp (point) indent-point 1 t))
+                  (error nil))))
+    ;; Point is sitting on first character of last (or count) sexp.
+    (cond ((= i 0)
+           (+ containing-form-column (* 2 lisp-body-indent)))
+          ((= i 1) (list normal-indent containing-form-start))
+          (t (+ containing-form-column lisp-body-indent)))))
+
+(put 'receive 'scheme-indent-function 'scheme-indent-receive)
+
+))        ; end of eval-after-load for scheme.el
