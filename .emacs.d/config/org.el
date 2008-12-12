e2bb5f6cba25710190a55dce0f8827ac2ab44a63
Andreas Rottmann <a.rottmann@gmx.at>
2008-12-12 20:17:59 +0100

    emacs (org): Added irclogs project

--- .emacs.d/config/org.el
+++ .emacs.d/config/org.el
@@ -5,3 +5,12 @@
 (setq org-log-done t)
 (setq org-agenda-files '("~/org/DiplomArbeit.org"))
 (setq org-CUA-compatible t)
+
+(setq org-publish-project-alist
+      '(("irclogs"
+	 :base-directory "~/src/spe/systems/irclogs/docs"
+	 :publishing-directory "~/src/spe/systems/irclogs/static"
+	 :section-numbers nil
+	 :table-of-contents nil
+	 :style "<link rel=\"stylesheet\" href=\"common.css\" type=\"text/css\" />
+                 <link rel=\"stylesheet\" href=\"docs.css\" type=\"text/css\" />")))
